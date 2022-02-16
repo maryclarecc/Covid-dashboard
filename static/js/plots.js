cc5e61ecf96418e29da27631c94e84aad4443c6b
maryclarecc <maryclarecc@gmail.com>
2022-02-15 20:31:12 -0600

    css and js

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,3 +1,4 @@
+// Map
 Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/us-population-density.json', function (data) {
 
     // Make codes uppercase to match the map data
@@ -65,3 +66,93 @@ Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/sam
         }]
     });
 });
+
+// Time Series
+d3.csv("https://github.com/maryclarecc/Covid-dashboard/blob/199fd99295fa7fc95cd677017b63c3e9816294b4/Resources/Covid_Vaccine_data_US_formatted.csv", function(err, rows){
+
+  function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
+
+var trace1 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'Daily Vaccines in US',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'Administered_Daily'),
+  line: {color: '#17BECF'}
+}
+
+// var trace2 = {
+//   type: "scatter",
+//   mode: "lines",
+//   name: 'AAPL Low',
+//   x: unpack(rows, 'Date'),
+//   y: unpack(rows, 'AAPL.Low'),
+//   line: {color: '#7F7F7F'}
+// }
+
+var data = [trace1];
+
+var layout = {
+  title: 'Daily Vaccines in the US',
+  xaxis: {
+    autorange: true,
+    range: ['2020-12-31', '2022-02-10'],
+    rangeselector: {buttons: [
+        {
+          count: 1,
+          label: '1m',
+          step: 'month',
+          stepmode: 'backward'
+        },
+        {
+          count: 6,
+          label: '6m',
+          step: 'month',
+          stepmode: 'backward'
+        },
+        {step: 'all'}
+      ]},
+    rangeslider: {range: ['2020-12-31', '2022-02-10']},
+    type: 'date'
+  },
+  yaxis: {
+    autorange: true,
+    range: [0, 2000000],
+    type: 'linear'
+  }
+};
+
+Plotly.newPlot('line', data, layout);
+})
+
+
+// Stacked Bar Chart
+var trace1 = {
+    x: ['giraffes', 'orangutans', 'monkeys'],
+    y: [20, 14, 23],
+    name: 'SF Zoo',
+    type: 'bar'
+  };
+  
+  var trace2 = {
+    x: ['giraffes', 'orangutans', 'monkeys'],
+    y: [12, 18, 29],
+    name: 'LA Zoo',
+    type: 'bar'
+  };
+
+  var trace3 = {
+    x: ['giraffes', 'orangutans', 'monkeys'],
+    y: [12, 18, 29],
+    name: 'LA Zoo',
+    type: 'bar'
+  };
+  
+  var data = [trace1, trace2, trace3];
+  
+  var layout = {barmode: 'stack'};
+  
+  Plotly.newPlot('stacked_bar', data, layout);
